0698e7127bea2f287121b8af269aadd4c2a222d5
ThanosSiopoudis <thanoulas@gmail.com>
2025-05-30 21:42:45 +0100

    sdk fixes

--- sdk/include/vmu_sdk_types.h
+++ sdk/include/vmu_sdk_types.h
@@ -6,7 +6,7 @@
 typedef struct sdk_api_t
 {
   void (*draw_pixel)(int x, int y, uint16_t color);
-  void (*log)(const char *fmt, ...);
+  void (*log)(const char *fmt, va_list args);
 } sdk_api_t;
 
 // Externally provided by the runtime loader
