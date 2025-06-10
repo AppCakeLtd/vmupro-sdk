56598be11578d7f65b733dd167c8a1c469a59593
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-06-10 21:23:27 +0100

    [VMUP-153] Remove older setup.sh in favour of packer.sh

--- tools/packer/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "VMUPacker: creating python3 venv"
-python3 -m venv .venv || exit 1
-source .venv/bin/activate
-pip install -r requirements.txt
-echo "VMUPacker env created"
-echo "VMUPacker: you can activate manually via .venv/Scripts/activate"
-echo "Ready"
