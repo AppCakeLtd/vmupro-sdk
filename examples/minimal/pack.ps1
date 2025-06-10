d49f81e9e62c73ddc410372f92e812af5d9c31a9
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-06-10 17:11:39 +0100

    [VMUP-153] Rename setup.ps1 packer.ps1

--- examples/minimal/pack.ps1
+++ examples/minimal/pack.ps1
@@ -1,2 +1,2 @@
 # get the venv running
-. ..\..\tools\packer\setup.ps1 -elfname vmupro_minimal -meta ./metadata.json -icon icon.bmp -debug true
+. ..\..\tools\packer\packer.ps1 -elfname vmupro_minimal -meta ./metadata.json -icon icon.bmp -debug true
