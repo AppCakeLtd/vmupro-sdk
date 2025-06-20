7d2f7866d001a8cdc1886949b799f8aad58ba6ce
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-06-20 10:39:44 +0100

    [VMUP-150] Update send.py and send.sh to put the binary in the apps folder

--- examples/minimal/send.sh
+++ examples/minimal/send.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 ../../tools/packer/send.py --func send --localfile "vmupro_minimal.vmupack" --remotefile "test.vmupack" --comport /dev/ttyYOURPATH --exec True
+python3 ../../tools/packer/send.py --func send --localfile "vmupro_minimal.vmupack" --remotefile "apps/vmupro_minimal.vmupack" --comport /dev/ttyYOURPATH --exec True
