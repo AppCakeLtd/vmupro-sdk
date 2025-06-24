e978f5d704e36bd2c2000246741a7407c6591c25
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-06-24 14:57:19 +0100

    [VMUP-150] Little derp - put minimal example back in /apps not /games

--- examples/minimal/send.ps1
+++ examples/minimal/send.ps1
@@ -1,2 +1,2 @@
 # python -m pip install pynput
-python ..\..\tools\packer\send.py --func send --localfile "vmupro_minimal.vmupack" --remotefile "games/vmupro_minimal.vmupack" --exec --monitor
+python ..\..\tools\packer\send.py --func send --localfile "vmupro_minimal.vmupack" --remotefile "apps/vmupro_minimal.vmupack" --exec --monitor
