dea786255be269d4d427db3202dc7217a22a9877
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-06-25 10:02:59 +0100

    [WIP] Kinda messy - dvd bounce example with raw image

--- examples/gfx_samples/main/graphics.c
+++ examples/gfx_samples/main/graphics.c
@@ -1,4 +1,5 @@
 #include "vmupro_sdk.h"
+#include "placeholder.h"
 
 const char *TAG = "[GFX Samples]";
 
@@ -13,32 +14,60 @@ void app_main(void)
 
   // Wait a bit to actually show the changes
   bool flipflop = false;
+
+  bool scroll_dir_x = true;
+  int scroll_pos_x = 10;
+  bool scroll_dir_y = true;
+  int scroll_pos_y = 24;
+
   while (true)
   {
 
     vmupro_color_t col = flipflop ? VMUPRO_COLOR_BLUE : VMUPRO_COLOR_RED;
+    // vmupro_color_t shapeCol = flipflop ? VMUPRO_COLOR_RED : VMUPRO_COLOR_BLUE;
     vmupro_display_clear(col);
-    if (flipflop)
+
+    vmupro_blit_buffer_at((uint8_t *)&placeholder, scroll_pos_x, scroll_pos_y, 76, 76);
+    vmupro_push_double_buffer_frame();
+
+    if (scroll_dir_x)
     {
-      vmupro_log(VMUPRO_LOG_INFO, TAG, "Blue");
+      scroll_pos_x++;
+      // let it go out of bounds so we see what happens)
+      if (scroll_pos_x >= 240 - 1)
+      {
+        scroll_dir_x = false;
+      }
     }
     else
     {
-      vmupro_log(VMUPRO_LOG_INFO, TAG, "Red");
+      scroll_pos_x--;
+      if (scroll_pos_x < 0)
+      {
+        scroll_dir_x = true;
+      }
     }
 
-    vmupro_color_t rectCol = flipflop ? VMUPRO_COLOR_RED : VMUPRO_COLOR_BLUE;
-    vmupro_draw_rect(10, 10, 100, 100, rectCol);
-    vmupro_push_double_buffer_frame();
+    if (scroll_dir_y)
+    {
+      scroll_pos_y++;
+      // let it go out of bounds so we see what happens)
+      if (scroll_pos_y >= 240 - 1)
+      {
+        scroll_dir_y = false;
+      }
+    }
+    else
+    {
+      scroll_pos_y--;
+      if (scroll_pos_y < 0)
+      {
+        scroll_dir_y = true;
+      }
+    }
 
     // Nice long delay so we know what should be drawn at any given time
-    vmupro_sleep_ms(1000);
-
-    vmupro_display_clear(col);
-    vmupro_draw_circle(90, 90, 30, rectCol);
-    vmupro_push_double_buffer_frame();
-
-    vmupro_sleep_ms(1000);
+    vmupro_sleep_ms(32);
 
     flipflop = !flipflop;
 
