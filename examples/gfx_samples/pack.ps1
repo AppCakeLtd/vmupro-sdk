187427713a5e4b5357c0437b993bb3e7fabeee4a
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-09-24 23:41:03 +0100

    [VMUP-174] Update pack.ps1 and pack.sh format a bit

--- examples/gfx_samples/pack.ps1
+++ examples/gfx_samples/pack.ps1
@@ -1,1 +1,1 @@
-. ..\..\tools\packer\packer.ps1 -elfname gfx_samples -meta ./metadata.json -icon icon.bmp -debug true
+. ..\..\tools\packer\packer.ps1 --elfname gfx_samples --meta ./metadata.json --icon icon.bmp --debug true
--- examples/gfx_samples/pack.sh
+++ examples/gfx_samples/pack.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-../../tools/packer/packer.sh . gfx_samples icon.bmp ./metadata.json true
+../../tools/packer/packer.sh --elfname gfx_samples --icon icon.bmp --meta ./metadata.json --debug true
