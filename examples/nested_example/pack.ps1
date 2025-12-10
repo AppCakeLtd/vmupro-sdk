4fb54822520e3e4cf2908aa5efb0442ce783c635
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2025-12-10 10:22:19 +0000

    Remove sdk version arg from .ps1 files to bring them in line with the .sh files

--- examples/nested_example/pack.ps1
+++ examples/nested_example/pack.ps1
@@ -1,1 +1,1 @@
-python ../../tools/packer/packer.py --projectdir . --appname nested_example --meta metadata.json --icon icon.bmp --sdkversion 1.0.0 --debug true
+python ../../tools/packer/packer.py --projectdir . --appname nested_example --meta metadata.json --icon icon.bmp --debug true
